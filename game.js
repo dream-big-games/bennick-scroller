e613e1f87b69f719473638ac87fd94d8dd5f6db2
621635Nicholas <621635@templetontitan.net>
2017-05-31 13:25:06 -0700

    Update game.js

--- game.js
+++ game.js
@@ -19,7 +19,7 @@ Aquaplane.Preloader.prototype = {
         this.load.bitmapFont('fat-and-tiny');
         this.load.bitmapFont('interfont');
 
-        this.load.images(['low-rez-corgi', 'low-rez-corgi', 'Blank', 'pole', 'rock', 'shark', 'sea' ]);
+        this.load.images(['Blank', 'pole', 'rock', 'shark', 'sea' ]);
         this.load.spritesheet('waves', 'waves.png', 16, 6);
         this.load.spritesheet('boat', 'low-rez-corgi.png', 128, 2);
 //        var sea = this.load.image('sea', 'sea.png');
